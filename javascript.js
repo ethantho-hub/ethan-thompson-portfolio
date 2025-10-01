eaa28499fe728f6638914f80fc58deca0e11520c
ethantho-hub <143546952+ethantho-hub@users.noreply.github.com>
2025-10-01 16:15:06 -0600

    good

---
+++ javascript.js
@@ -0,0 +1,9 @@
+function turnOrange(){
+    document.body.style.backgroundColor = "orange";
+}
+function turnBlue(){
+    document.body.style.backgroundColor = "blue";
+}
+function turnBack(){
+    document.body.style.backgroundColor = "forestgreen";
+}
